258daecd1c66b44cf6c1f2837e350575f6034f09
penkovoleg <purtrtggggg@gmail.com>
2022-06-19 23:03:03 +0600

    added validation

--- src/main/java/com/example/socks/model/Socks.java
+++ src/main/java/com/example/socks/model/Socks.java
@@ -3,6 +3,10 @@ package com.example.socks.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "socks")
@@ -14,12 +18,17 @@ public class Socks {
     private Long id;
 
     @Column(name = "color")
+    @NotNull
+    @Size(min = 3, message = "Color should be at least 3 characters")
     private String color;
 
     @Column(name = "cotton_part")
+    @Min(value = 0, message = "Cotton part should be at least 0 percent")
+    @Max(value = 100, message = "Cotton part should be no more 100 percent")
     private int cottonPart;
 
     @Column(name = "quantity")
+    @Min(value = 1, message = "Quantity should be at least 1 pair")
     private int quantity;
 
 }
