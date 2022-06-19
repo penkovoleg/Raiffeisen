33ec22fcfe323b439562494355b40b862487dca2
penkovoleg <purtrtggggg@gmail.com>
2022-06-19 23:04:54 +0600

    removed extra method "findByColor"

--- src/main/java/com/example/socks/repository/SocksRepository.java
+++ src/main/java/com/example/socks/repository/SocksRepository.java
@@ -1,15 +1,13 @@
 package com.example.socks.repository;
 
 import com.example.socks.model.Socks;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 @Repository
 public interface SocksRepository extends JpaRepository<Socks, Long> {
-
-    List<Socks> findByColor(String color);
 
     Socks findByColorAndCottonPart(String color, int cottonPart);
 
