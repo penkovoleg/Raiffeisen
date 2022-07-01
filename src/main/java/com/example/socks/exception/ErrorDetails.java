1c125c71a64d2069af3a57f56c52152abc4df571
penkovoleg <purtrtggggg@gmail.com>
2022-07-01 22:36:39 +0600

    added Builder

--- src/main/java/com/example/socks/exception/ErrorDetails.java
+++ src/main/java/com/example/socks/exception/ErrorDetails.java
@@ -1,22 +1,26 @@
 package com.example.socks.exception;
 
-import lombok.Data;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
-@Data
 @AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class ErrorDetails {
 
     private Date timestamp;
 
     private int status;
 
-    private String details;
+    private String error;
 
     private String message;
 
-    private String error;
+    private String details;
 
 }
